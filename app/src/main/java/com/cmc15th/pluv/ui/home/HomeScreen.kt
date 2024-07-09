226998aab012f57f26d53c8d65a4900119830381
MinhoiKoo <minlove2013@gmail.com>
2024-07-10 07:54:03 +0900

    feat: 플레이리스트 목록 UI 구현

--- app/src/main/java/com/cmc15th/pluv/ui/home/HomeScreen.kt
+++ app/src/main/java/com/cmc15th/pluv/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.cmc15th.pluv.ui.home
 
+import android.content.Context
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,20 +13,112 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.cmc15th.pluv.R
+
+@Composable
+fun PlayListRowArea(
+    context: Context,
+    @StringRes title: Int,
+    playlistItems: List<String>,
+    modifier: Modifier = Modifier,
+    onAddClick: () -> Unit
+) {
+    PlayListName(title, modifier = modifier, onAddClick = onAddClick)
+    Spacer(modifier = Modifier.size(13.dp))
+    PlayListRow(context = context, playListItems = playlistItems, modifier = modifier)
+}
+
+@Composable
+fun PlayListName(
+    @StringRes title: Int,
+    modifier: Modifier = Modifier,
+    onAddClick: () -> Unit
+) {
+    Box(modifier = modifier) {
+        Text(
+            text = stringResource(id = title),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.align(Alignment.CenterStart)
+        )
+        Text(
+            text = stringResource(id = R.string.show_more),
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Normal,
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .clickable { onAddClick() },
+        )
+    }
+}
+
+@Composable
+fun PlayListRow(
+    context: Context,
+    playListItems: List<String>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(playListItems) { playList ->
+            PlayListItem(
+                context = context,
+                imageUrl = playList,
+                modifier = Modifier.size(140.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun PlayListItem(
+    context: Context,
+    imageUrl: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.clip(shape = MaterialTheme.shapes.small)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(context = context)
+                .data(imageUrl)
+                .crossfade(true)
+                .build(),
+            contentDescription = "Migrated Play List Image",
+            modifier = Modifier.fillMaxSize()
+        )
+        Icon(
+            imageVector = Icons.Default.PlayArrow,
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(5.dp)
+        )
+    }
+}
 
 @Composable
 fun MigrationMethodColumn(
