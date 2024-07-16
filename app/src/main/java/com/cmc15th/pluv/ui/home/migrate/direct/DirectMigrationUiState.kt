765ca0360ba0bac7895f6405149758cebf87c798
MinhoiKoo <minlove2013@gmail.com>
2024-07-16 21:31:33 +0900

    feat: 로딩 상태 및 플레이리스트 목록, 선택한 플레이리스트 UiState 추가

--- app/src/main/java/com/cmc15th/pluv/ui/home/migrate/direct/DirectMigrationUiState.kt
+++ app/src/main/java/com/cmc15th/pluv/ui/home/migrate/direct/DirectMigrationUiState.kt
@@ -1,11 +1,15 @@
 package com.cmc15th.pluv.ui.home.migrate.direct
 
+import com.cmc15th.pluv.core.model.Playlist
 import com.cmc15th.pluv.domain.model.PlayListApp
 import com.cmc15th.pluv.domain.model.PlayListApp.Companion.getAllPlaylistApps
 
 data class DirectMigrationUiState(
+    val isLoading: Boolean = false,
     val sourceApps: List<PlayListApp> = getAllPlaylistApps(),
     val destinationApps: List<PlayListApp> = emptyList(),
     val selectedSourceApp: PlayListApp = PlayListApp.EMPTY,
     val selectedDestinationApp: PlayListApp = PlayListApp.EMPTY,
+    val allPlaylists: List<Playlist> = emptyList(),
+    val selectedPlaylist: Long = -1L
 )
