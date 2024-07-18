beb0aa62a1216b53169e8bb799d4908608536acf
MinhoiKoo <minlove2013@gmail.com>
2024-07-18 17:16:40 +0900

    refactor: 아전 단계 표시하는 Topbar Component로 분리 및 NavHost에서 Screen에 Column 제거

--- app/src/main/java/com/cmc15th/pluv/PLUVNavHost.kt
+++ app/src/main/java/com/cmc15th/pluv/PLUVNavHost.kt
@@ -1,26 +1,8 @@
 package com.cmc15th.pluv
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
@@ -33,12 +15,14 @@ import com.cmc15th.pluv.ui.home.HomeScreen
 import com.cmc15th.pluv.ui.home.migrate.direct.DisplayMigrationPathScreen
 import com.cmc15th.pluv.ui.home.migrate.direct.SelectDestinationAppScreen
 import com.cmc15th.pluv.ui.home.migrate.direct.SelectMigratePlaylistScreen
+import com.cmc15th.pluv.ui.home.migrate.direct.SelectMigrationMusicScreen
 import com.cmc15th.pluv.ui.home.migrate.direct.SelectSourceAppScreen
 
 @Composable
 fun PLUVNavHost(
     navController: NavHostController
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
 
     NavHost(
         navController = navController,
@@ -54,127 +38,120 @@ fun PLUVNavHost(
         }
 
         navigation(
-            startDestination = DestinationScreens.SelectApp.route,
-            route = DestinationScreens.DirectMigrationRoot.route
             route = DestinationScreens.DirectMigrationRoot.route,
             startDestination = DestinationScreens.DirectMigrationSelectSourceApp.route
         ) {
+            val totalSteps = DirectMigrationRoutes.getRouteSize()
 
             composable(route = DestinationScreens.DirectMigrationSelectSourceApp.route) {
-                Column {
-                    TopBarWithProgress(navController = navController)
-                    Spacer(modifier = Modifier.height(28.dp))
-                    SelectSourceAppScreen(
-                        viewModel = navController.sharedViewModel(route = DestinationScreens.DirectMigrationRoot.route),
-                        navigateToSelectDestinationApp = {
-                            navController.navigate(DestinationScreens.DirectMigrationSelectDestinationApp.route)
+                val currentRoute = navBackStackEntry?.destination?.route
+                SelectSourceAppScreen(
+                    currentStep = DirectMigrationRoutes.getCurrentStep(currentRoute),
+                    totalStep = totalSteps,
+                    onCloseClick = {
+                        navController.navigate(DestinationScreens.Home.route) {
+                            popUpTo(DestinationScreens.Home.route) { inclusive = true }
                         }
-                    )
-                }
+                    },
+                    viewModel = navController.sharedViewModel(route = DestinationScreens.DirectMigrationRoot.route),
+                    navigateToSelectDestinationApp = {
+                        navController.navigate(DestinationScreens.DirectMigrationSelectDestinationApp.route)
+                    }
+                )
             }
 
             composable(route = DestinationScreens.DirectMigrationSelectDestinationApp.route) {
-                Column {
-                    TopBarWithProgress(navController = navController)
-                    Spacer(modifier = Modifier.height(28.dp))
-                    SelectDestinationAppScreen(
-                        viewModel = navController.sharedViewModel(route = DestinationScreens.DirectMigrationRoot.route),
-                        navigateToSelectSource = {
-                            navController.navigate(DestinationScreens.DirectMigrationSelectSourceApp.route) {
-                                popUpTo(DestinationScreens.DirectMigrationSelectSourceApp.route) { inclusive = true }
-                            }
-                        },
-                        navigateToDisplayMigrationPath = {
-                            navController.navigate(DestinationScreens.ExecuteDirectMigration.route)
+                val currentRoute = navBackStackEntry?.destination?.route
+                SelectDestinationAppScreen(
+                    currentStep = DirectMigrationRoutes.getCurrentStep(currentRoute),
+                    totalStep = totalSteps,
+                    onCloseClick = {
+                        navController.navigate(DestinationScreens.Home.route) {
+                            popUpTo(DestinationScreens.Home.route) { inclusive = true }
                         }
-                    )
-                }
+                    },
+                    viewModel = navController.sharedViewModel(route = DestinationScreens.DirectMigrationRoot.route),
+                    navigateToSelectSource = {
+                        navController.navigate(DestinationScreens.DirectMigrationSelectSourceApp.route) {
+                            popUpTo(DestinationScreens.DirectMigrationSelectSourceApp.route) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    navigateToDisplayMigrationPath = {
+                        navController.navigate(DestinationScreens.ExecuteDirectMigration.route)
+                    }
+                )
             }
 
             composable(route = DestinationScreens.ExecuteDirectMigration.route) {
-                Column {
-                    TopBarWithProgress(navController = navController)
-                    Spacer(modifier = Modifier.height(28.dp))
-                    DisplayMigrationPathScreen(
-                        viewModel = navController.sharedViewModel(route = DestinationScreens.DirectMigrationRoot.route),
-                        navigateToSelectDestinationApp = {
-                            navController.navigate(DestinationScreens.DirectMigrationSelectDestinationApp.route) {
-                                popUpTo(DestinationScreens.DirectMigrationSelectDestinationApp.route) { inclusive = true }
-                            }
-                        },
-                        navigateToSelectPlaylist = {
-                            navController.navigate(DestinationScreens.SelectMigratePlaylist.route)
+                val currentRoute = navBackStackEntry?.destination?.route
+                DisplayMigrationPathScreen(
+                    currentStep = DirectMigrationRoutes.getCurrentStep(currentRoute),
+                    totalStep = totalSteps,
+                    onCloseClick = {
+                        navController.navigate(DestinationScreens.Home.route) {
+                            popUpTo(DestinationScreens.Home.route) { inclusive = true }
                         }
-                    )
-                }
+                    },
+                    viewModel = navController.sharedViewModel(route = DestinationScreens.DirectMigrationRoot.route),
+                    navigateToSelectDestinationApp = {
+                        navController.navigate(DestinationScreens.DirectMigrationSelectDestinationApp.route) {
+                            popUpTo(DestinationScreens.DirectMigrationSelectDestinationApp.route) {
+                                inclusive = true
+                            }
+                        }
+                    },
+                    navigateToSelectPlaylist = {
+                        navController.navigate(DestinationScreens.SelectMigratePlaylist.route)
+                    }
+                )
+
             }
 
             composable(route = DestinationScreens.SelectMigratePlaylist.route) {
-                Column {
-                    TopBarWithProgress(navController = navController)
-                    Spacer(modifier = Modifier.height(28.dp))
-                    SelectMigratePlaylistScreen(
-                        navigateToDisplayMigrationPath = {
-                            navController.navigate(DestinationScreens.ExecuteDirectMigration.route) {
-                                popUpTo(DestinationScreens.ExecuteDirectMigration.route) { inclusive = true }
+                val currentRoute = navBackStackEntry?.destination?.route
+                SelectMigratePlaylistScreen(
+                    currentStep = DirectMigrationRoutes.getCurrentStep(currentRoute),
+                    totalStep = totalSteps,
+                    onCloseClick = {
+                        navController.navigate(DestinationScreens.Home.route) {
+                            popUpTo(DestinationScreens.Home.route) { inclusive = true }
+                        }
+                    },
+                    navigateToDisplayMigrationPath = {
+                        navController.navigate(DestinationScreens.ExecuteDirectMigration.route) {
+                            popUpTo(DestinationScreens.ExecuteDirectMigration.route) {
+                                inclusive = true
                             }
-                        },
-                        viewModel = navController.sharedViewModel(route = DestinationScreens.DirectMigrationRoot.route)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun TopBarWithProgress(navController: NavController) {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val parentRoute = navBackStackEntry?.destination?.parent?.route
-    val totalSteps = 5
-    val currentStep = when (parentRoute) {
-        DestinationScreens.SelectApp.route -> 1
-        DestinationScreens.SelectMigratePlyListRoot.route -> 2
-        DestinationScreens.ExecuteDirectMigration.route -> 3
-        else -> 0
-    }
-    val progress = currentStep.toFloat() / totalSteps
-
-    Column {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-                .padding(start = 8.dp, end = 24.dp, top = 11.dp, bottom = 11.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            IconButton(onClick = { navController.popBackStack() }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.leftarrow),
-                    contentDescription = "Back",
-                    modifier = Modifier.size(24.dp)
+                        }
+                    },
+                    navigateToSelectMigrationMusic = {
+                        navController.navigate(DestinationScreens.SelectMigrationMusic.route)
+                    },
+                    viewModel = navController.sharedViewModel(route = DestinationScreens.DirectMigrationRoot.route)
                 )
             }
-            Row {
-                Text(
-                    text = "$currentStep",
-                    color = colorResource(id = R.color.current_step_color)
-                )
-                Text(
-                    text = "/$totalSteps",
-                    color = Color.Black
+            composable(route = DestinationScreens.SelectMigrationMusic.route) {
+                val currentRoute = navBackStackEntry?.destination?.route
+                SelectMigrationMusicScreen(
+                    currentStep = DirectMigrationRoutes.getCurrentStep(currentRoute),
+                    totalStep = totalSteps,
+                    onCloseClick = {
+                        navController.navigate(DestinationScreens.Home.route) {
+                            popUpTo(DestinationScreens.Home.route) { inclusive = true }
+                        }
+                    },
+                    navigateToSelectPlaylist = {
+                        navController.navigate(DestinationScreens.SelectMigratePlaylist.route)
+                    },
+                    navigateToExecuteMigrationScreen = {
+
+                    },
+                    viewModel = navController.sharedViewModel(route = DestinationScreens.DirectMigrationRoot.route)
                 )
             }
         }
-        LinearProgressIndicator(
-            progress = progress,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(4.dp),
-            color = colorResource(id = R.color.current_step_color),
-            trackColor = Color.LightGray,
-        )
     }
 }
 
@@ -184,5 +161,36 @@ inline fun <reified T : ViewModel> NavController.sharedViewModel(route: String):
         getBackStackEntry(route)
     }
     return hiltViewModel(parentEntry)
+}
+
+/** 플레이리스트 직접 이전하기 Navigation Route (위에 LinearProgressBar 표시하기 위한 Object) */
+object DirectMigrationRoutes {
+
+    private val selectSourceAndDestinationRoute = listOf(
+        DestinationScreens.DirectMigrationSelectSourceApp.route,
+        DestinationScreens.DirectMigrationSelectDestinationApp.route,
+        DestinationScreens.ExecuteDirectMigration.route
+    )
+
+    private val directMigrationRoutes = listOf(
+        selectSourceAndDestinationRoute,
+        listOf(
+            DestinationScreens.SelectMigratePlaylist.route
+        ),
+        listOf(
+            DestinationScreens.SelectMigrationMusic.route
+        )
+    )
+
+    fun getRouteSize(): Int = directMigrationRoutes.size
+
+    fun getCurrentStep(currentRoute: String?): Int {
+        directMigrationRoutes.forEachIndexed { index, routeList ->
+            if (routeList.contains(currentRoute)) {
+                return index + 1  // 단계는 1부터 시작
+            }
+        }
+        return 0
+    }
 }
 
---
+++ app/src/main/java/com/cmc15th/pluv/core/designsystem/component/TopAppBar.kt
@@ -0,0 +1,66 @@
+package com.cmc15th.pluv.core.designsystem.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cmc15th.pluv.R
+
+@Composable
+fun TopBarWithProgress(
+    modifier: Modifier = Modifier,
+    totalStep: Int,
+    currentStep: Int,
+    onCloseClick: () -> Unit
+) {
+
+    val progress = currentStep.toFloat() / totalStep
+
+    Column {
+        IconButton(
+            onClick = {
+                onCloseClick()
+            },
+            modifier = Modifier
+                .align(Alignment.End)
+                .padding(horizontal = 14.dp, vertical = 7.dp)
+                .size(32.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "Close",
+            )
+        }
+        LinearProgressIndicator(
+            progress = progress,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(4.dp),
+            color = colorResource(id = R.color.current_step_color),
+            trackColor = Color.LightGray,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TopBarWithProgressPreview() {
+    TopBarWithProgress(
+        totalStep = 5,
+        currentStep = 1,
+        onCloseClick = {}
+    )
+}
