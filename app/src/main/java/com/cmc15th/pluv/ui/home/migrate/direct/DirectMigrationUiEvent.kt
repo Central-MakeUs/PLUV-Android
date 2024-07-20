01d8dba641c0cfe22adead246bb02e5f7af95601
MinhoiKoo <minlove2013@gmail.com>
2024-07-21 03:56:25 +0900

    feat: 음악 개별 선택/헤제, 전체 선택/해제 UiEvnet 정의 및 동작 구현

--- app/src/main/java/com/cmc15th/pluv/ui/home/migrate/direct/DirectMigrationUiEvent.kt
+++ app/src/main/java/com/cmc15th/pluv/ui/home/migrate/direct/DirectMigrationUiEvent.kt
@@ -8,4 +8,5 @@ sealed class DirectMigrationUiEvent {
     data object ExecuteMigration : DirectMigrationUiEvent()
     data object SelectPlaylist : DirectMigrationUiEvent()
     class SelectMusic(val selectedMusicId: Long) : DirectMigrationUiEvent()
+    class SelectAllMusic(val selectAllFlag: Boolean) : DirectMigrationUiEvent()
 }
--- app/src/main/java/com/cmc15th/pluv/ui/home/migrate/direct/DirectMigrationViewModel.kt
+++ app/src/main/java/com/cmc15th/pluv/ui/home/migrate/direct/DirectMigrationViewModel.kt
@@ -1,7 +1,9 @@
 package com.cmc15th.pluv.ui.home.migrate.direct
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.cmc15th.pluv.core.model.Music
 import com.cmc15th.pluv.domain.model.PlayListApp
 import com.cmc15th.pluv.domain.model.PlayListApp.Companion.getAllPlaylistApps
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,12 +11,14 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DirectMigrationViewModel @Inject constructor(): ViewModel() {
+class DirectMigrationViewModel @Inject constructor() : ViewModel() {
+
     private val _uiState: MutableStateFlow<DirectMigrationUiState> =
         MutableStateFlow(DirectMigrationUiState())
     val uiState: StateFlow<DirectMigrationUiState> = _uiState.asStateFlow()
@@ -49,11 +53,35 @@ class DirectMigrationViewModel @Inject constructor(): ViewModel() {
             is DirectMigrationUiEvent.SelectSourceApp -> {
                 setSelectedSourceApp(event.selectedApp)
             }
+
             is DirectMigrationUiEvent.SelectDestinationApp -> {
                 setSelectedDestinationApp(event.selectedApp)
             }
+
             is DirectMigrationUiEvent.ExecuteMigration -> {
-                // Todo: Implement migration logic
+                //TODO Source App Login
+                fetchPlaylists()
+            }
+
+            is DirectMigrationUiEvent.SelectPlaylist -> {
+                fetchMusicByPlaylist()
+            }
+
+            is DirectMigrationUiEvent.SelectMusic -> {
+                selectedMusics.value = selectedMusics.value.toMutableList().apply {
+                    if (contains(event.selectedMusicId)) {
+                        remove(event.selectedMusicId)
+                    } else {
+                        add(event.selectedMusicId)
+                    }
+                }
+            }
+
+            is DirectMigrationUiEvent.SelectAllMusic -> {
+                when (event.selectAllFlag) {
+                    true -> selectedMusics.value = emptyList()
+                    false -> selectedMusics.value = uiState.value.allMusics.map { it.id }
+                }
             }
         }
     }
