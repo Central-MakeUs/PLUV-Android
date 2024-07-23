a2078da3252469207cc5c8cb0fa3c1e8e1f8a6ba
MinhoiKoo <minlove2013@gmail.com>
2024-07-23 17:40:39 +0900

    refactor: nullable 변경

--- app/src/main/java/com/cmc15th/pluv/core/model/ApiResult.kt
+++ app/src/main/java/com/cmc15th/pluv/core/model/ApiResult.kt
@@ -5,13 +5,13 @@ import java.io.IOException
 /**
  * 네트워크 응답을 래핑하는 인터페이스
  */
-sealed interface ApiResult<out T : Any> {
-    data class Success<out T : Any>(val data: T?) : ApiResult<T>
+sealed interface ApiResult<out T> {
+    data class Success<out T>(val data: T) : ApiResult<T>
     data class Failure(val code: Int, val error: String?) : ApiResult<Nothing>
     data class NetworkError(val exception: IOException) : ApiResult<Nothing>
     data class Unexpected(val t: Throwable?) : ApiResult<Nothing>
 
-    fun <R : Any> map(transform: (T?) -> R?): ApiResult<R> {
+    fun <R : Any> map(transform: (T) -> R): ApiResult<R> {
         return when (this) {
             is Success -> Success(transform(data))
             is Failure -> this
