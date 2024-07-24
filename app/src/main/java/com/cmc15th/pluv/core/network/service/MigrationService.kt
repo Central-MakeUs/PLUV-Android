50c1037d39a9cd85b3cb5748f5cabba32b9eb8ce
MinhoiKoo <minlove2013@gmail.com>
2024-07-24 16:55:48 +0900

    refactor: 하드코딩 제거

--- app/src/main/java/com/cmc15th/pluv/core/network/service/MigrationService.kt
+++ app/src/main/java/com/cmc15th/pluv/core/network/service/MigrationService.kt
@@ -3,13 +3,16 @@ package com.cmc15th.pluv.core.network.service
 import com.cmc15th.pluv.core.model.ApiResult
 import com.cmc15th.pluv.core.network.request.PlaylistAccessToken
 import com.cmc15th.pluv.core.network.response.ReadPlaylistResponse
+import com.cmc15th.pluv.domain.model.PlayListApp
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface MigrationService {
 
-    @POST("spotify/playLists/read")
+    @POST("{source}/playLists/read")
     suspend fun fetchPlaylists(
+        @Path("source") source: PlayListApp,
         @Body accessToken: PlaylistAccessToken
     ): ApiResult<List<ReadPlaylistResponse>>
 
