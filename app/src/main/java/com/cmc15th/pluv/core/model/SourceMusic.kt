2e0447f09cc76afa7bea367592748a6da4c517ab
MinhoiKoo <minlove2013@gmail.com>
2024-07-26 02:25:28 +0900

    remove

--- app/src/main/java/com/cmc15th/pluv/core/model/SourceMusic.kt
+++
@@ -1,8 +0,0 @@
-package com.cmc15th.pluv.core.model
-
-data class SourceMusic(
-    val isrcCode: String = "",
-    val thumbNailUrl: String = "",
-    val title: String = "",
-    val artistName: String = "",
-)
