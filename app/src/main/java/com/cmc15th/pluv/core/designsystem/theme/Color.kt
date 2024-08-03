9ba013a27952eb015d538c024ea3fe9786cdcdfa
MinhoiKoo <minlove2013@gmail.com>
2024-08-03 21:21:12 +0900

    design: Color 정의

--- app/src/main/java/com/cmc15th/pluv/core/designsystem/theme/Color.kt
+++ app/src/main/java/com/cmc15th/pluv/core/designsystem/theme/Color.kt
@@ -9,3 +9,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryDefault = Color(0xFF9E22FF)
+val PrimaryDisabled = Color(0xFFDBACFF)
+val PrimaryBg = Color(0xFFFBF5FF)
+
+val Gray100 = Color(0xFFF7F7F7)
+val Gray200 = Color(0xFFF2F2F2)
+val Gray300 = Color(0xFFDEDEDE)
+val Gray400 = Color(0xFFB0B0B0)
+val Gray500 = Color(0xFF878787)
+val Gray600 = Color(0xFF5C5C5C)
+val Gray700 = Color(0xFF3D3D3D)
+val Gray800 = Color(0xFF1E1E1E)
+
