e204605ef68c83262ecb9688b1f9815c3aab944c
MinhoiKoo <minlove2013@gmail.com>
2024-08-18 19:58:29 +0900

    feat: 마이페이지 관련 ViewModel 구현 #23

---
+++ app/src/main/java/com/cmc15th/pluv/ui/mypage/viewmodel/MypageUiState.kt
@@ -0,0 +1,8 @@
+package com.cmc15th.pluv.ui.mypage.viewmodel
+
+data class MypageUiState(
+    val nickName: String = "음악듣는 원숭이",
+    val email: List<String> = emptyList(),
+    val modifiedNickName: String = "",
+    val isNicknameModifying: Boolean = false
+)
