5b217d6acc75053243f27e0de9e0f3c4d2b3e228
MinhoiKoo <minlove2013@gmail.com>
2024-08-19 03:18:08 +0900

    chore: versionCode plus

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.cmc15th.pluv"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
