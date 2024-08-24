963f2499426885d6c9323e94f437d1027ab912cc
MinhoiKoo <minlove2013@gmail.com>
2024-08-24 15:43:37 +0900

    design: 홈 화면 디자인 수정

--- app/src/main/java/com/cmc15th/pluv/ui/home/HomeScreen.kt
+++ app/src/main/java/com/cmc15th/pluv/ui/home/HomeScreen.kt
@@ -1,10 +1,10 @@
 package com.cmc15th.pluv.ui.home
 
-import android.content.Context
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,32 +13,39 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.cmc15th.pluv.R
+import com.cmc15th.pluv.core.designsystem.component.PlaylistCard
+import com.cmc15th.pluv.core.designsystem.theme.Content1
+import com.cmc15th.pluv.core.designsystem.theme.Content2
+import com.cmc15th.pluv.core.designsystem.theme.Gray200
+import com.cmc15th.pluv.core.designsystem.theme.Gray400
+import com.cmc15th.pluv.core.designsystem.theme.Gray500
+import com.cmc15th.pluv.core.designsystem.theme.Gray600
+import com.cmc15th.pluv.core.designsystem.theme.HomePurple
+import com.cmc15th.pluv.core.designsystem.theme.HomeWhite
+import com.cmc15th.pluv.core.designsystem.theme.PrimaryDefault
+import com.cmc15th.pluv.core.designsystem.theme.Title2
+import com.cmc15th.pluv.core.designsystem.theme.Title4
 
 @Composable
 fun HomeScreen(
@@ -58,128 +65,183 @@ fun HomeScreen(
 
     Column(
         modifier = modifier
-            .padding(24.dp)
+            .fillMaxSize()
+            .background(Gray200)
             .verticalScroll(scrollState)
     ) {
-        Text(
-            text = stringResource(id = R.string.app_name),
-            fontSize = 24.sp,
-            fontWeight = FontWeight.SemiBold,
-            color = colorResource(id = R.color.app_logo_color)
-        )
-        Spacer(modifier = Modifier.size(19.dp))
-        MigrationMethodColumn(
+
+        Column(
             modifier = Modifier
-                .fillMaxWidth(),
-            onDirectClick = {
-                navigateToDirectMigration()
-            },
-            onScreenShotClick = {
-                navigateToScreenShotMigration()
-            }
-        )
-        Spacer(modifier = Modifier.size(50.dp))
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(listOf(HomePurple, HomeWhite)),
+                )
+                .padding(horizontal = 16.dp)
+        ) {
+            Spacer(modifier = Modifier.size(45.dp))
+
+            Icon(
+                modifier = Modifier
+                    .width(66.dp)
+                    .height(24.dp),
+                painter = painterResource(id = R.drawable.pluvlogo),
+                tint = PrimaryDefault,
+                contentDescription = "Pluv Logo"
+            )
+
+            Spacer(modifier = Modifier.size(19.dp))
+            MigrationMethodColumn(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onDirectClick = {
+                    navigateToDirectMigration()
+                },
+                onScreenShotClick = {
+                    navigateToScreenShotMigration()
+                }
+            )
+            Spacer(modifier = Modifier.size(50.dp))
+        }
+
         PlayListRowArea(
-            context = context,
             title = R.string.migrated_play_list,
             playlistItems = playlistItems,
             modifier = Modifier.fillMaxWidth(),
             onAddClick = {}
         )
-        Spacer(modifier = Modifier.size(45.dp))
-        PlayListRowArea(
-            context = context,
-            title = R.string.saved_play_list,
-            playlistItems = playlistItems,
-            modifier = Modifier.fillMaxWidth(),
-            onAddClick = {}
+        Spacer(modifier = Modifier.size(12.dp))
+        PlayListRow(
+            playListItems = listOf(
+                "https://picsum.photos/120/120?random=1",
+                "https://picsum.photos/120/120?random=1",
+                "https://picsum.photos/120/120?random=1",
+            ),
+            description = "저장한 플레이리스트",
+            modifier = Modifier.fillMaxWidth().background(Color.White)
         )
+
+//        Spacer(modifier = Modifier.size(45.dp))
+//        PlayListRowArea(
+//            context = context,
+//            title = R.string.saved_play_list,
+//            playlistItems = playlistItems,
+//            modifier = Modifier.fillMaxWidth(),
+//            onAddClick = {}
+//        )
     }
 }
 
 @Composable
 fun PlayListRowArea(
-    context: Context,
     @StringRes title: Int,
     playlistItems: List<String>,
     modifier: Modifier = Modifier,
     onAddClick: () -> Unit
 ) {
-    PlayListName(title, modifier = modifier, onAddClick = onAddClick)
-    Spacer(modifier = Modifier.size(13.dp))
-    PlayListRow(context = context, playListItems = playlistItems, modifier = modifier)
-}
-
-@Composable
-fun PlayListName(
-    @StringRes title: Int,
-    modifier: Modifier = Modifier,
-    onAddClick: () -> Unit
-) {
-    Box(modifier = modifier) {
-        Text(
-            text = stringResource(id = title),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.SemiBold,
-            modifier = Modifier.align(Alignment.CenterStart)
-        )
-        Text(
-            text = stringResource(id = R.string.show_more),
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Normal,
+    // Content inside the rounded container
+    Column(
+        modifier = modifier
+            .clip(RoundedCornerShape(topEnd = 24.dp, topStart = 24.dp))
+            .background(Color.White)
+    ) {
+        Spacer(modifier = Modifier.size(24.dp))
+        Icon(
+            painter = painterResource(id = R.drawable.my_playlist),
+            contentDescription = "MyPlaylist",
             modifier = Modifier
-                .align(Alignment.CenterEnd)
-                .clickable { onAddClick() },
+                .padding(horizontal = 16.dp)
+                .width(108.dp)
+                .height(20.dp),
+            tint = Gray400
+        )
+        PlayListRow(
+            playListItems = listOf(
+                "https://picsum.photos/120/120?random=1",
+                "https://picsum.photos/120/120?random=1",
+                "https://picsum.photos/120/120?random=1",
+            ),
+            description = "최근 옮긴 항목",
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
         )
     }
 }
 
 @Composable
 fun PlayListRow(
-    context: Context,
     playListItems: List<String>,
+    description: String,
     modifier: Modifier = Modifier
 ) {
-    LazyRow(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    Column(
+        modifier = modifier
     ) {
-        items(playListItems) { playList ->
-            PlayListItem(
-                context = context,
-                imageUrl = playList,
-                modifier = Modifier.size(140.dp)
-            )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = description, style = Title4, modifier = Modifier.padding(vertical = 24.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "전체보기", style = Content2, color = Gray600)
+                Icon(
+                    painterResource(id = R.drawable.rightarrow),
+                    contentDescription = null,
+                    modifier = Modifier.size(20.dp)
+                )
+            }
         }
+
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            items(playListItems) { playList ->
+                PlaylistCard(
+                    imageUrl = playList,
+                    modifier = Modifier
+                        .size(140.dp),
+                    shape = RoundedCornerShape(8.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.size(24.dp))
     }
 }
 
-@Composable
-fun PlayListItem(
-    context: Context,
-    imageUrl: String,
-    modifier: Modifier = Modifier
-) {
-    Box(
-        modifier = modifier.clip(shape = MaterialTheme.shapes.small)
-    ) {
-        AsyncImage(
-            model = ImageRequest.Builder(context = context)
-                .data(imageUrl)
-                .crossfade(true)
-                .build(),
-            contentDescription = "Migrated Play List Image",
-            modifier = Modifier.fillMaxSize()
-        )
-        Icon(
-            imageVector = Icons.Default.PlayArrow,
-            contentDescription = null,
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(5.dp)
-        )
-    }
-}
+//@Composable
+//fun PlayListItem(
+//    context: Context,
+//    imageUrl: String,
+//    modifier: Modifier = Modifier
+//) {
+//    Box(
+//        modifier = modifier.clip(shape = MaterialTheme.shapes.small)
+//    ) {
+//        AsyncImage(
+//            model = ImageRequest.Builder(context = context)
+//                .data(imageUrl)
+//                .crossfade(true)
+//                .build(),
+//            contentDescription = "Migrated Play List Image",
+//            modifier = Modifier.fillMaxSize()
+//        )
+//        Icon(
+//            imageVector = Icons.Default.PlayArrow,
+//            contentDescription = null,
+//            modifier = Modifier
+//                .align(Alignment.BottomEnd)
+//                .padding(5.dp)
+//        )
+//    }
+//}
 
 @Composable
 fun MigrationMethodColumn(
@@ -192,13 +254,17 @@ fun MigrationMethodColumn(
     ) {
         MigrationMethodItem(
             R.string.migration_direct,
+            icon = R.drawable.direct_icon,
             onClick = onDirectClick,
+            description = stringResource(id = R.string.migration_direct_description),
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.size(12.dp))
         MigrationMethodItem(
             R.string.migration_screenshot,
+            icon = R.drawable.screenshot_icon,
             onClick = onScreenShotClick,
+            description = stringResource(id = R.string.migration_screenshot_description),
             modifier = Modifier.fillMaxWidth()
         )
     }
@@ -207,47 +273,56 @@ fun MigrationMethodColumn(
 @Composable
 fun MigrationMethodItem(
     @StringRes text: Int,
+    @DrawableRes icon: Int,
+    description: String,
     modifier: Modifier = Modifier,
     onClick: () -> Unit
 ) {
     Card(
-        modifier = modifier.height(108.dp),
+        modifier = modifier
+            .height(108.dp)
+            .clickable { onClick() },
+        shape = RoundedCornerShape(20.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White)
     ) {
-        Box(
-            modifier = modifier
-                .clickable(onClick = onClick)
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween,
+
+            Icon(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 18.dp)
+                    .padding(16.dp)
+                    .size(74.dp),
+                painter = painterResource(id = icon),
+                tint = Color.Unspecified,
+                contentDescription = "Migration Method Icon"
+            )
+
+            Column(
+                verticalArrangement = Arrangement.Center,
             ) {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier.padding(start = 20.dp)
+                Text(
+                    text = stringResource(id = text),
+                    style = Title2,
+                )
+                Spacer(modifier = Modifier.size(12.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
                     Text(
-                        text = stringResource(id = text),
-                        fontSize = 22.sp,
-                        fontWeight = FontWeight.SemiBold,
+                        text = description,
+                        style = Content1,
+                        color = Gray500
                     )
-                    Spacer(modifier = Modifier.size(12.dp))
-                    Text(
-                        text = "음원 스트리밍 서비스 연동",
-                        fontSize = 14.sp,
-                        color = colorResource(id = R.color.sub_title_gray_color)
+                    Spacer(modifier = Modifier.size(4.dp))
+                    Icon(
+                        painterResource(id = R.drawable.rightarrow),
+                        contentDescription = null,
+                        modifier = Modifier.size(10.dp)
                     )
                 }
-                Icon(
-                    painter = painterResource(id = R.drawable.rightarrow),
-                    contentDescription = null,
-                    modifier = Modifier.size(24.dp) // 아이콘 크기 조정
-                )
             }
         }
     }
