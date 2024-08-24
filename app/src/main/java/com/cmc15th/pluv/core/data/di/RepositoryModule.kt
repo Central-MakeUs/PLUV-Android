34af7115a057bcb440d639fcba692ca649dd6a4e
MinhoiKoo <minlove2013@gmail.com>
2024-08-25 04:59:33 +0900

    feat: Feed, Member Di 구성

--- app/src/main/java/com/cmc15th/pluv/core/data/di/RepositoryModule.kt
+++ app/src/main/java/com/cmc15th/pluv/core/data/di/RepositoryModule.kt
@@ -2,8 +2,12 @@ package com.cmc15th.pluv.core.data.di
 
 import com.cmc15th.pluv.core.data.repository.AuthRepository
 import com.cmc15th.pluv.core.data.repository.AuthRepositoryImpl
+import com.cmc15th.pluv.core.data.repository.FeedRepository
+import com.cmc15th.pluv.core.data.repository.FeedRepositoryImpl
 import com.cmc15th.pluv.core.data.repository.LoginRepository
 import com.cmc15th.pluv.core.data.repository.LoginRepositoryImpl
+import com.cmc15th.pluv.core.data.repository.MemberRepository
+import com.cmc15th.pluv.core.data.repository.MemberRepositoryImpl
 import com.cmc15th.pluv.core.data.repository.PlaylistRepository
 import com.cmc15th.pluv.core.data.repository.PlaylistRepositoryImpl
 import dagger.Binds
@@ -33,4 +37,16 @@ abstract class RepositoryModule {
     abstract fun bindAuthRepository(
         authRepositoryImpl: AuthRepositoryImpl
     ): AuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindFeedRepository(
+        feedRepositoryImpl: FeedRepositoryImpl
+    ): FeedRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindMemberRepository(
+        memberRepositoryImpl: MemberRepositoryImpl
+    ): MemberRepository
 }
--- app/src/main/java/com/cmc15th/pluv/core/network/di/ApiModule.kt
+++ app/src/main/java/com/cmc15th/pluv/core/network/di/ApiModule.kt
@@ -1,6 +1,8 @@
 package com.cmc15th.pluv.core.network.di
 
+import com.cmc15th.pluv.core.network.service.FeedService
 import com.cmc15th.pluv.core.network.service.LoginService
+import com.cmc15th.pluv.core.network.service.MemberService
 import com.cmc15th.pluv.core.network.service.MigrationService
 import dagger.Module
 import dagger.Provides
@@ -22,5 +24,15 @@ object ApiModule {
     @Singleton
     fun provideLoginService(@NetworkModule.BaseClient retrofit: Retrofit): LoginService =
         retrofit.create(LoginService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideFeedService(@NetworkModule.AuthenticatedClient retrofit: Retrofit): FeedService =
+        retrofit.create(FeedService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideMemberService(@NetworkModule.AuthenticatedClient retrofit: Retrofit): MemberService =
+        retrofit.create(MemberService::class.java)
 
 }
