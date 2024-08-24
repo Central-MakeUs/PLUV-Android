0348dab155675d51899442160f52d20894dfdef1
MinhoiKoo <minlove2013@gmail.com>
2024-08-25 04:57:06 +0900

    feat: 유저 회원탈퇴 화면 구현

--- app/src/main/java/com/cmc15th/pluv/ui/mypage/viewmodel/MypageViewModel.kt
+++ app/src/main/java/com/cmc15th/pluv/ui/mypage/viewmodel/MypageViewModel.kt
@@ -2,21 +2,32 @@ package com.cmc15th.pluv.ui.mypage.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.cmc15th.pluv.core.data.repository.MemberRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MypageViewModel @Inject constructor() : ViewModel() {
+class MypageViewModel @Inject constructor(
+    private val memberRepository: MemberRepository
+) : ViewModel() {
     private val _uiState: MutableStateFlow<MypageUiState> = MutableStateFlow(MypageUiState())
     val uiState: StateFlow<MypageUiState> = _uiState.asStateFlow()
 
     private val _uiEvent: MutableSharedFlow<MypageUiEvent> = MutableSharedFlow()
+
+    private val _uiEffect: Channel<MypageUiEffect> = Channel()
+    val uiEffect: Flow<MypageUiEffect> = _uiEffect.receiveAsFlow()
+
+
 
     init {
         subscribeEvents()
@@ -49,6 +60,18 @@ class MypageViewModel @Inject constructor() : ViewModel() {
                 changeNickname()
                 //TODO 닉네임 변경 API 호출
             }
+            is MypageUiEvent.OnUnRegisterMemberClicked -> {
+                unRegisterMember()
+            }
+            is MypageUiEvent.OnUnregisterChecked -> {
+                _uiState.update { it.copy(isUnregisterChecked = it.isUnregisterChecked.not()) }
+            }
+        }
+    }
+
+    private fun sendEffect(effect: MypageUiEffect) {
+        viewModelScope.launch {
+            _uiEffect.send(effect)
         }
     }
 
@@ -67,6 +90,20 @@ class MypageViewModel @Inject constructor() : ViewModel() {
     private fun changeNickname() {
         _uiState.update {
             it.copy(nickName = _uiState.value.modifiedNickName)
+        }
+    }
+
+    private fun unRegisterMember() {
+        viewModelScope.launch {
+            memberRepository.unRegisterMember().collect { result ->
+                result.onSuccess {
+                    sendEffect(MypageUiEffect.NavigateToLogin)
+                }
+
+                result.onFailure { i, s ->
+                    sendEffect(MypageUiEffect.OnFailure(s!!))
+                }
+            }
         }
     }
 }
