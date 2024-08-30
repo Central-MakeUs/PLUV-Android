7eca6e3a03ab1381b1ba17d9a403111b601a117f
MinhoiKoo <minlove2013@gmail.com>
2024-08-30 21:32:19 +0900

    feat: 연동한 계정 목록 추가

---
+++ app/src/main/java/com/cmc15th/pluv/core/model/SocialAccount.kt
@@ -0,0 +1,6 @@
+package com.cmc15th.pluv.core.model
+
+enum class SocialAccount {
+    google,
+    spotify
+}
--- app/src/main/java/com/cmc15th/pluv/ui/mypage/viewmodel/MypageUiState.kt
+++ app/src/main/java/com/cmc15th/pluv/ui/mypage/viewmodel/MypageUiState.kt
@@ -1,9 +1,12 @@
 package com.cmc15th.pluv.ui.mypage.viewmodel
 
+import com.cmc15th.pluv.core.model.SocialAccount
+
 data class MypageUiState(
     val nickName: String = "음악듣는 원숭이",
     val email: List<String> = emptyList(),
     val modifiedNickName: String = "",
     val isNicknameModifying: Boolean = false,
+    val integratedSocialSocialAccount: List<SocialAccount> = emptyList(),
     val isUnregisterChecked: Boolean = false,
 )
