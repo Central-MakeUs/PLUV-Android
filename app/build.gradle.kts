249ed3d518aa9db540fedcbae48020ff2d1f3006
MinhoiKoo <minlove2013@gmail.com>
2024-09-08 16:01:43 +0900

    chore: 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.cmc15th.pluv"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.0.1"
+        versionCode = 12
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
