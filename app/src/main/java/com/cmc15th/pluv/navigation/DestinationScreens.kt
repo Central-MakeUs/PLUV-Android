66c0ce7678dce8fb78d6018f19e6ced02cdd9e81
MinhoiKoo <minlove2013@gmail.com>
2024-09-12 19:31:51 +0900

    refactor: 플레이리스트 이전 화면들 MigrationRoute로 분리

--- app/src/main/java/com/cmc15th/pluv/navigation/DestinationScreens.kt
+++ app/src/main/java/com/cmc15th/pluv/navigation/DestinationScreens.kt
@@ -3,9 +3,7 @@ package com.cmc15th.pluv.navigation
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed interface DestinationScreens {
-    @Serializable
-    data object Splash : DestinationScreens
+internal sealed interface DestinationScreens {
 
     @Serializable
     data object Onboarding : DestinationScreens
@@ -17,54 +15,12 @@ sealed interface DestinationScreens {
     data object Login : DestinationScreens
 
     @Serializable
-    data object DirectMigrationRoot : DestinationScreens
-
-    @Serializable
-    data object DirectMigrationSelectSourceApp : DestinationScreens
-
-    @Serializable
-    data object DirectMigrationSelectDestinationApp : DestinationScreens
-
-    @Serializable
-    data object ExecuteDirectMigration : DestinationScreens
-
-    @Serializable
-    data object SelectMigratePlaylist : DestinationScreens
-
-    @Serializable
-    data object SelectMigrationMusic : DestinationScreens
-
-    @Serializable
-    data object SelectSimilarMusic : DestinationScreens
-
-    @Serializable
-    data object ShowNotFoundMusic : DestinationScreens
-
-    @Serializable
-    data object UploadPlaylistScreenShot : DestinationScreens
-
-    @Serializable
-    data object MigrationProcess : DestinationScreens
-
-    @Serializable
-    data object MigratedResult : DestinationScreens
-
-    @Serializable
-    data object Feed : DestinationScreens
-
-    @Serializable
     data class FeedInfo(
         val feedId: Long
     ) : DestinationScreens
 
     @Serializable
-    data object SavedFeedRoot : DestinationScreens
-
-    @Serializable
     data object SavedFeed : DestinationScreens
-
-    @Serializable
-    data object SavedFeedDetail : DestinationScreens
 
     @Serializable
     data object UserInfo : DestinationScreens
@@ -79,23 +35,74 @@ sealed interface DestinationScreens {
     data object Unregister : DestinationScreens
 
     @Serializable
-    data object History : DestinationScreens
-
-    @Serializable
     data object AllHistory : DestinationScreens
 
     @Serializable
     data class HistoryDetail(
         val historyId: Long
     ) : DestinationScreens
+
+    @Serializable
+    data object MigrationRoute : DestinationScreens {
+        @Serializable
+        data object DirectMigrationSelectSourceApp : DestinationScreens
+
+        @Serializable
+        data object DirectMigrationSelectDestinationApp : DestinationScreens
+
+        @Serializable
+        data object ExecuteDirectMigration : DestinationScreens
+
+        @Serializable
+        data object SelectMigratePlaylist : DestinationScreens
+
+        @Serializable
+        data object SelectMigrationMusic : DestinationScreens
+
+        @Serializable
+        data object SelectSimilarMusic : DestinationScreens
+
+        @Serializable
+        data object ShowNotFoundMusic : DestinationScreens
+
+        @Serializable
+        data object UploadPlaylistScreenShot : DestinationScreens
+
+        @Serializable
+        data object MigrationProcess : DestinationScreens
+
+        @Serializable
+        data object MigratedResult : DestinationScreens
+
+        val routes = listOf(
+            DirectMigrationSelectSourceApp,
+            UploadPlaylistScreenShot,
+            DirectMigrationSelectDestinationApp,
+            ExecuteDirectMigration,
+            SelectMigratePlaylist,
+            SelectMigrationMusic,
+            SelectSimilarMusic,
+            ShowNotFoundMusic,
+            MigrationProcess,
+            MigratedResult
+        )
+
+        fun currentStep(route: DestinationScreens?): Int {
+            val step = routes.indexOf(route)
+            return if (step <= 1) 1
+            else step + 1
+        }
+    }
 }
 
 @Serializable
-sealed interface BottomTabRoute : DestinationScreens {
+internal sealed interface BottomTabRoute : DestinationScreens {
     @Serializable
     data object Home : BottomTabRoute
+
     @Serializable
     data object Feed : BottomTabRoute
+
     @Serializable
     data object Mypage : BottomTabRoute
 }
